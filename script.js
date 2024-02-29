9a7c4242c89088b4e63409d6b84fa0a4d324757a
Munteanu <munteanuvictoria1@gmail.com>
2024-02-29 15:13:24 +0100

    update

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ this. calcAge = function(){
 }
 }
 
-const victoria = new Person('Victoria', 2034);
+const victoria = new Person('Victoria', 2059);
 
 
 //a new object is created
@@ -27,5 +27,34 @@ const whoElse = new Person('Nobody', 2038);
 console.log(romain, whoElse);
 console.log(victoria instanceof Person); // true 
 
+
+
+// Prototypes   every function in JS has a prototype function 
+
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function (){
+ console.log(2046 - this.birthYear);
+}; 
+
+victoria.calcAge();
+console.log(victoria.__proto__ === Person.prototype); 
+// the prototype here is what's going to be used wiht the person constructor function 
+//.prototypeOfLinkedObjects 
+//New operator is creating the proto property 
+
+Person.prototype.species  = "Homo sapiens";
+console.log(victoria.species, romain.species);
+
+const arr = [1, 2, 3, 4, 5,6,6, 5, 5,5, 7,8,9]; 
+
+Array.prototype.unique = function(){
+  return [...new Set(this)];
+} 
+console.log(arr.unique());
+
+
+const h1 = document.querySelector ('h1'); 
+console.dir( x => x+1); 
 
 
