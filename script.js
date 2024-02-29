dbd9e60100fb77452d33167e3b337cb2d24d6f21
Munteanu <munteanuvictoria1@gmail.com>
2024-02-29 11:49:15 +0100

    update

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+//Constructor functions  
+//Instance properties 
+const Person = function(firstName, birthYear){
+this.firstname = firstName; 
+this.birthYear = birthYear;
+
+
+//Never do this!!! 
+this. calcAge = function(){
+    console.log(2034 - this.birthYear);
+}
+}
+
+const victoria = new Person('Victoria', 2034);
+
+
+//a new object is created
+//a function is clalles, this = {}
+// {} linked ot a prototype 
+//function automatically returns {}
+
+
+const romain = new Person('Romain', 1991); 
+const whoElse = new Person('Nobody', 2038); 
+console.log(romain, whoElse);
+console.log(victoria instanceof Person); // true 
+
+
+
